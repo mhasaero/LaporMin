4956463de4ba7e4e4c793a8c8fd6dcd923b8dc82
mhasaero <eroouji7314@gmail.com>
2025-03-20 14:58:54 +0700

    build: tailwind in vite.config.js

--- resources/js/Pages/Login.jsx
+++ resources/js/Pages/Login.jsx
@@ -1,8 +1,11 @@
+import "../../css/app.css";
+
 export default function Login() {
     return (
         <section id="login">
             <div>
-                <h1>Login</h1>
+                <h1 className="text-red-500 text-2xl">Login</h1>
+                <div></div>
             </div>
         </section>
     );
