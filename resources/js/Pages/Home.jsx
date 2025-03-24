03e584b94b298c9eda5bbabfb6684d532d39c2c7
mhasaero <eroouji7314@gmail.com>
2025-03-24 14:44:07 +0700

    build: add schadcn

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
         <section id="home">
             <div>
                 <h1>Home</h1>
+                <Button className="bg-pink-400">Halo</Button>
             </div>
         </section>
     );
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ use App\Http\Controllers\RegisterController;
 Route::get('/', [HomeController::class, 'view']);
 Route::get('/login', [LoginController::class, 'view']);
 Route::get('/register', [RegisterController::class, 'view']);
+
+Route::redirect('/here', '/there');
