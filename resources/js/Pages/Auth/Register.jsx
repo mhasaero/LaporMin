bb595f591d9346bbd8fb0e7241125ca0de8363b7
RA. Nur Afifah Widyadhana <widyadhana92@gmail.com>
2025-03-26 23:44:29 +0700

    feat: add form at register

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -24,6 +24,35 @@ export default function Register() {
     return (
         <GuestLayout>
             <Head title="Register" />
+            <section className="flex justify-end items-end h-screen w-full bg-[#23318C]">
+                <div className="w-1/3 h-screen bg-white flex flex-col items-center justify-center">
+                    <h1 className="font-bold text-2xl">Sign Up</h1>
+                    <form className="pt-3 flex flex-col gap-6">
+                        <div className="">
+                            <input
+                                type="text"
+                                className="w-80 rounded-3xl px-6 py-2"
+                                name="email"
+                                id="email"
+                                placeholder="email unsri"
+                            />
+                        </div>
+                        <div className="mt-3 flex justify-center">
+                            <button
+                                type="submit"
+                                className="w-80 rounded-3xl px-6 py-2 bg-[#23318C] text-white"
+                            >
+                                sign up
+                            </button>
+                        </div>
+                    </form>
+                    <div className="text-center mt-4 font-weight-light">
+                        <Link to="/login" className="text-primary">
+                            or sign in ?
+                        </Link>
+                    </div>
+                </div>
+            </section>
         </GuestLayout>
     );
 }
