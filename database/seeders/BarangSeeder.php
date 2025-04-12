9bce0d251dccdb9a14330a5c932f758a69c5964e
adityaagym <adityaakmal35@gmail.com>
2025-04-12 16:59:36 +0700

    Feat:add table barang

---
+++ database/seeders/BarangSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BarangSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('barang')->insert([
+            [
+                'nama_barang' => 'Test',
+                'kategori' => 'Test',
+                'deskripsi' => 'Tidak Test',
+                'stok' => 2,
+                'status' => 'Tersedia',
+                'lokasi' => 'Lab A',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+    }
+}
