421ef4d837ee468310c69dc54cc91a6a4fd07deb
mhasaero <eroouji7314@gmail.com>
2025-04-21 13:41:08 +0700

    fix(box): change design for box index

--- resources/js/Pages/Box/Index.tsx
+++ resources/js/Pages/Box/Index.tsx
@@ -1,5 +1,6 @@
 import BoxLayout from "@/Layouts/BoxLayout";
 import { Head } from "@inertiajs/react";
+import { Button } from "@/Components/ui/button";
 
 export default function Index() {
     return (
@@ -7,12 +8,6 @@ export default function Index() {
             <Head title="Box Peminjaman" />
             <section className="flex flex-col ">
                 <div className="flex flex-col w-full gap-16 h-screen mt-24 ">
-                    <div className="shadow-lg border-1 border-black p-6">
-                        <label className="text-2xl space-x-4">
-                            <input type="checkbox" className="size-5"></input>
-                            <span>Barang</span>
-                        </label>
-                    </div>
                     <div className="">
                         <div className="shadow-lg border-1 border-black p-6">
                             <label className="text-2xl space-x-4">
@@ -20,10 +15,10 @@ export default function Index() {
                                     type="checkbox"
                                     className="size-5"
                                 ></input>
-                                <span>Kategori</span>
+                                <span>Barang yang Dipinjam</span>
                             </label>
                         </div>
-                        <div className="shadow-lg border-b-1 border-s-1 border-e-1 border-black p-12">
+                        <div className="flex items-center justify-between shadow-lg border-b-1 border-s-1 border-e-1 border-black p-12">
                             <label className="flex items-center text-2xl space-x-4">
                                 <input
                                     type="checkbox"
@@ -35,45 +30,34 @@ export default function Index() {
                                     <span>Barang 1</span>
                                 </div>
                             </label>
+                            <div className="flex items-center gap-6 text-2xl">
+                                <Button className="text-xl">+</Button>
+                                <span>1</span>
+                                <Button className="text-xl">-</Button>
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-between shadow-lg border-b-1 border-s-1 border-e-1 border-black p-12">
+                            <label className="flex items-center text-2xl space-x-4">
+                                <input
+                                    type="checkbox"
+                                    className="size-5"
+                                ></input>
+                                <div className="flex items-center gap-4">
+                                    <img src="" alt="" />
+                                    <div className="size-24 border-2 border-black"></div>
+                                    <span>Barang 2</span>
+                                </div>
+                            </label>
+                            <div className="flex items-center gap-6 text-2xl">
+                                <Button className="text-xl">+</Button>
+                                <span>1</span>
+                                <Button className="text-xl">-</Button>
+                            </div>
                         </div>
                     </div>
-                    <div className="">
-                        <div className="shadow-lg border-1 border-black p-6">
-                            <label className="text-2xl space-x-4">
-                                <input
-                                    type="checkbox"
-                                    className="size-5"
-                                ></input>
-                                <span>Kategori</span>
-                            </label>
-                        </div>
-                        <div className="shadow-lg border-b-1 border-s-1 border-e-1 border-black p-12">
-                            <label className="flex items-center text-2xl space-x-4">
-                                <input
-                                    type="checkbox"
-                                    className="size-5"
-                                ></input>
-                                <div className="flex items-center gap-4">
-                                    <img src="" alt="" />
-                                    <div className="size-24 border-2 border-black"></div>
-                                    <span>Barang 1</span>
-                                </div>
-                            </label>
-                        </div>
-                        <div className="shadow-lg border-b-1 border-s-1 border-e-1 border-black p-12">
-                            <label className="flex items-center text-2xl space-x-4">
-                                <input
-                                    type="checkbox"
-                                    className="size-5"
-                                ></input>
-                                <div className="flex items-center gap-4">
-                                    <img src="" alt="" />
-                                    <div className="size-24 border-2 border-black"></div>
-                                    <span>Barang 1</span>
-                                </div>
-                            </label>
-                        </div>
-                    </div>
+                    <Button className="self-end px-12 text-2xl h-14">
+                        konfirmasi Pinjam barang
+                    </Button>
                 </div>
             </section>
         </BoxLayout>
